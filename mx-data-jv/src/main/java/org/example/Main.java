043cacfd103a88214f3f128a93fecebfae10a698
Rajesh <rajesh.hofo@gmail.com>
2024-05-22 00:49:42 +0800

    fix: code refactored

--- mx-data-jv/src/main/java/org/example/Main.java
+++ mx-data-jv/src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import tech.tablesaw.api.IntColumn;
-import tech.tablesaw.api.Row;
 import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 
@@ -12,22 +11,38 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    static String filePath = "/Users/vampire/Documents/sellde/mox_test/mx-data-test/src/main/resources/input.txt";
+    static String filePath = "/Users/vampire/Documents/sellde/mox_test/mx-data-jv/src/main/resources/input.txt";
 
     public static void main(String[] args) {
         try {
             var table = readFixedWidthFile(filePath);
 
+            /* step 1 : create a new column for domain name more than & equal 20 characters , and filter the data*/
+            var domain = table.stringColumn("EMAIL").map(Main::isValidDomainLengthMoreThan20);
+            domain.setName("DOMAIN_LENGTH_20+");
+            table.addColumns(domain);
+
+            // filter the data using selection
+            var domainLengthSelection = domain.isEqualTo("true");
+            table = table.where(domainLengthSelection);
+
+
+            /* step 2 : create a new column for id first char lowercase validation , and filter the data*/
+            var idFirstChar = table.stringColumn("ID").map(Main::isFirstCharLowercase);
+            idFirstChar.setName("id_first_char_lower");
+            table.addColumns(idFirstChar);
+
+            // filter the data for first character is not lower
+            var idFirstCharSelection = idFirstChar.isEqualTo("false");
+            table = table.where(idFirstCharSelection);
+
+
             var expectedCodesList = table
-                    /* Sort the data by email*/
+                    /* step 3:  finally, Sort the data by email*/
                     .sortAscendingOn("EMAIL")
+                    /* filter only first 5 records */
+                    .first(5)
                     .stream()
-                    /* validate the domain name length*/
-                    .filter(Main::isValidDomainLengthMoreThan20)
-                    /* validate userid first character is in lowercase or not */
-                    .filter(row -> !isFirstCharLowercase(row))
-                    /* fetch only first 5 records */
-                    .limit(5)
                     .map(row -> row.getInt("CODE"))
                     .toList();
 
@@ -75,8 +90,8 @@ public class Main {
         return Table.create("FixedWidthData", emailColumn, usernameColumn, idColumn, codeColumn);
     }
 
-    private static boolean isValidDomainLengthMoreThan20(Row row) {
-        var email = row.getString(0);
+
+    private static String isValidDomainLengthMoreThan20(String email) {
         if (email == null || !email.contains("@")) {
             throw new IllegalArgumentException("Invalid email address");
         }
@@ -85,12 +100,11 @@ public class Main {
         String domain = email.substring(email.indexOf('@') + 1);
 
         // Return domain length
-        return domain.length() >= 20;
+        return String.valueOf(domain.length() >= 20);
     }
 
 
-    private static boolean isFirstCharLowercase(Row row) {
-        var id = row.getString(2);
+    private static String isFirstCharLowercase(String id) {
         if (id == null || id.isEmpty()) {
             throw new IllegalArgumentException("The string is null or empty");
         }
@@ -99,8 +113,9 @@ public class Main {
         char firstChar = id.charAt(0);
 
         // Check if the first character is a lowercase letter
-        return Character.isLowerCase(firstChar);
+        return String.valueOf(Character.isLowerCase(firstChar));
     }
-}
 
+
+}
 
